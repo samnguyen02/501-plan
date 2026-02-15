d9fdc66c34e9d3c678a80ec17a65c64ad6898b9f
samnguyen02 <samnguyen02@tamu.edu>
2026-02-15 03:22:23 -0600

    manager view is locked behind oauth

--- app/controllers/admins/omniauth_callbacks_controller.rb
+++ app/controllers/admins/omniauth_callbacks_controller.rb
@@ -19,7 +19,7 @@ class Admins::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || root_path
+    stored_location_for(resource_or_scope) || manager_index_path
   end
 
   private
